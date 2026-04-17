9f9c36a4d17c5fd18ee33295bf4f4e7c8c4c38b9
C4L3B-C41C3D0 <*caleb.caicedo7067@itse.ac.pa*>
2026-04-16 19:04:57 -0600

    Examen final

--- QuizGame.java
+++ QuizGame.java
@@ -1,12 +1,12 @@
 import java.util.Scanner;
 
 public class QuizGame {
-    public static void main(String[] args) {
+    public static void main (String [] args){
         Scanner sc = new Scanner(System.in);
         char option;
 
-        do {
-            System.out.println("=== QUIZ DE FÚTBOL, TECNOLOGÍA E HISTORIA ===");
+        do { 
+            System.out.println("===Juego de Adivinanza===");
             System.out.println("Elige nivel de dificultad:");
             System.out.println("1) Fácil");
             System.out.println("2) Medio");
@@ -17,58 +17,58 @@ public class QuizGame {
 
             if (level == 1) {
                 // Preguntas fáciles
-                System.out.println("Fútbol: ¿Quién es conocido como 'La Pulga'?");
-                System.out.println("A) Cristiano Ronaldo  B) Lionel Messi  C) Neymar");
+                System.out.println("¿Qué significa 'CPU'?");
+                System.out.println("A) Central Processing Unit  B) Computer Personal Utility  C) Control Program Unit");
                 char ans = sc.next().toUpperCase().charAt(0);
-                if (ans == 'B') score++;
+                if (ans == 'A') score++;
 
-                System.out.println("Tecnología: ¿Qué empresa creó Windows?");
+                System.out.println("¿Qué empresa creó Windows?");
                 System.out.println("A) Apple  B) Microsoft  C) Google");
                 ans = sc.next().toUpperCase().charAt(0);
                 if (ans == 'B') score++;
 
-                System.out.println("Historia: ¿Quién descubrió América en 1492?");
-                System.out.println("A) Cristóbal Colón  B) Simón Bolívar  C) Hernán Cortés");
+                System.out.println("¿Qué navegador pertenece a Google?");
+                System.out.println("A) Safari  B) Chrome  C) Edge");
                 ans = sc.next().toUpperCase().charAt(0);
-                if (ans == 'A') score++;
+                if (ans == 'B') score++;
 
             } else if (level == 2) {
                 // Preguntas medias
-                System.out.println("Fútbol: ¿Qué país ganó el Mundial 2014?");
-                System.out.println("A) Alemania  B) Brasil  C) Argentina");
+                System.out.println("¿Qué significa 'HTML'?");
+                System.out.println("A) HyperText Markup Language  B) High Tech Modern Logic  C) Home Tool Management List");
                 char ans = sc.next().toUpperCase().charAt(0);
                 if (ans == 'A') score++;
 
-                System.out.println("Tecnología: ¿Qué significa 'HTML'?");
-                System.out.println("A) HyperText Markup Language  B) High Tech Modern Logic  C) Home Tool Management List");
+                System.out.println("Qué empresa desarrolló el sistema operativo Android?");
+                System.out.println("A) Apple  B) Google  C) Microsoft");
                 ans = sc.next().toUpperCase().charAt(0);
-                if (ans == 'A') score++;
+                if (ans == 'B') score++;
 
-                System.out.println("Historia: ¿En qué año cayó el Muro de Berlín?");
-                System.out.println("A) 1989  B) 1975  C) 1999");
+                System.out.println("¿Qué lenguaje se usa principalment para páginas web dinámicas?");
+                System.out.println("A) JavaScript  B) Python  C) C++");
                 ans = sc.next().toUpperCase().charAt(0);
                 if (ans == 'A') score++;
 
             } else if (level == 3) {
-                // Preguntas difíciles
-                System.out.println("Fútbol: ¿Qué jugador tiene más goles en Mundiales?");
-                System.out.println("A) Miroslav Klose  B) Pelé  C) Ronaldo Nazário");
+                //Preguntas difíciles 
+                System.out.println("¿Quién es considerado el padre de la computación?");
+                System.out.println("A) Alan Turing B) Charles Babbage  C) Bill Gates");
                 char ans = sc.next().toUpperCase().charAt(0);
-                if (ans == 'A') score++;
-
-                System.out.println("Tecnología: ¿Quién es considerado el padre de la computación?");
-                System.out.println("A) Alan Turing  B) Charles Babbage  C) Bill Gates");
-                ans = sc.next().toUpperCase().charAt(0);
                 if (ans == 'B') score++;
 
-                System.out.println("Historia: ¿En qué año comenzó la Primera Guerra Mundial?");
-                System.out.println("A) 1914  B) 1939  C) 1929");
+                System.out.println("¿Qué protocolo se usa para transferir archivos en internet?");
+                System.out.println("A) FTP  B) SMTP  C) HTTP");
+                ans = sc.next().toUpperCase().charAt(0);
+                if (ans == 'A') score++;
+
+                System.out.println("¿En qué año se lanzó la primera versión de Java?");
+                System.out.println("A) 1995  B) 2000  C) 1989");
                 ans = sc.next().toUpperCase().charAt(0);
                 if (ans == 'A') score++;
             }
-
-            // Mostrar puntaje final
-            System.out.println("\nTu puntaje final: " + score);
+        
+            //mostrar puntaje final 
+            System.out.println("\nTu puntaje final:" + score);
             if (score == 3) {
                 System.out.println("¡Excelente, todas correctas!");
             } else if (score == 2) {
@@ -79,12 +79,11 @@ public class QuizGame {
                 System.out.println("Necesitas estudiar más, ¡ánimo!");
             }
 
-            // Preguntar si quiere jugar otra vez
+            //Preguntar si quiere jugar otra vez
             System.out.println("\n¿Quieres jugar otra vez? (y/n)");
             option = sc.next().charAt(0);
 
-        } while (option == 'y' || option == 'Y');
-
+        } while (option == 'y'|| option == 'Y');
         System.out.println("Gracias por jugar.");
         sc.close();
     }
